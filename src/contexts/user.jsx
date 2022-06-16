4b6cab09bc53f4db527e47773bf26ca60ab8f2b1
Levan Pantsulaia <levanpantsulaia@yahoo.com>
2022-06-16 18:05:04 +0400

    can't remember what changed, but need the latest version to continue on PC

--- src/contexts/user.jsx
+++ src/contexts/user.jsx
@@ -1,25 +1,25 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
-import { onAuthStateChanged, createUserDocumentFromAuth } from 'firebase/auth';
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils';
 
 export const UserContext = createContext({
-  currentUser: null,
   setCurrentUser: () => null,
+  currentUser: null,
 });
 
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
 
-  signOutUser();
-
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged((user) => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
         createUserDocumentFromAuth(user);
       }
       setCurrentUser(user);
     });
+
+    return unsubscribe;
   }, []);
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
